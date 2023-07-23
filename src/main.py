a718006525c2576e2df0b3fb69f13107df4d4282
TheNefelin <metalflap@gmail.com>
2023-07-23 13:57:27 -0400

    update

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from sql_server import getTesting, getData
 
 app = FastAPI(title="Kartax", description="API", version="3.0")
 
@@ -7,4 +8,12 @@ async def root():
   return [
     { "msge": "Kartax API v3.0", "swagger": "add '/docs' to the url"}
   ]
+
+@app.get("/testing")
+def testing():
+  return getTesting()
+
+@app.get("/negocio")
+async def negocio_get_all():
+  return await getData("EXECUTE pa_negocio_get_active")
 

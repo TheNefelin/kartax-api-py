95ab59f98713e7edc071f09ab99d4b262aae446d
TheNefelin <metalflap@gmail.com>
2023-07-23 16:32:53 -0400

    update

--- sql/sql_server.py
+++ sql/sql_server.py
@@ -9,7 +9,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_DATABASE = os.getenv("DB_DATABASE")
 DB_USER = os.getenv("DB_USER")
 DB_PASWORD = os.getenv("DB_PASWORD")
-DB_PATH = "DRIVER={" + DB_DRIVER + "};SERVER=" + DB_HOST + ";DATABASE=" + DB_DATABASE + ";UID=" + DB_USER + ";PWD=" + DB_PASWORD
+DB_PATH = "DRIVER={" + DB_DRIVER + "};SERVER=" + DB_HOST + ";DATABASE=" + DB_DATABASE + ";UID=" + DB_USER + ";PWD=" + DB_PASWORD + ";Trusted_Connection=yes"
 
 def getTesting():
   return [

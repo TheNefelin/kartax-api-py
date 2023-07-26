9c199dd921dab700b86f368f890a419ba2332903
TheNefelin <metalflap@gmail.com>
2023-07-25 21:13:28 -0400

    update

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 app = FastAPI(title="Kartax", description="API", version="3.0")
 
-# model ---------------------------------
+# model -------------------------------------------------------------------
 class Registrarse(BaseModel):
   nombres: str
   apellidos: str
@@ -25,13 +25,13 @@ class Usuario(BaseModel):
 
 fechaql = datetime.strptime("20230725 15:42:00", "%Y%m%d %H:%M:%S")
 
-usuarios = [
-  Usuario(id=1, nombres="Francsico", apellidos="Carmona", usuario="NEFELIN", correo="flcarmonac@yahoo.com", fecha=fechaql, is_active=True),
-  Usuario(id=2, nombres="Nicolas", apellidos="Carmona", usuario="KRATOS", correo="kratos@yahoo.com", fecha=fechaql, is_active=True),
-  Usuario(id=3, nombres="Michael", apellidos="Carmona", usuario="ZUMO", correo="zumo@yahoo.com", fecha=fechaql, is_active=True),
-]
+# usuarios = [
+#   Usuario(id=1, nombres="Francsico", apellidos="Carmona", usuario="NEFELIN", correo="flcarmonac@yahoo.com", fecha=fechaql, is_active=True),
+#   Usuario(id=2, nombres="Nicolas", apellidos="Carmona", usuario="KRATOS", correo="kratos@yahoo.com", fecha=fechaql, is_active=True),
+#   Usuario(id=3, nombres="Michael", apellidos="Carmona", usuario="ZUMO", correo="zumo@yahoo.com", fecha=fechaql, is_active=True),
+# ]
 
-# crud ----------------------------------
+# root --------------------------------------------------------------------
 @app.get("/")
 async def root():
   return getTesting()
@@ -48,14 +48,25 @@ async def registrarse(prop: Registrarse):
   params = (prop.nombres, prop.apellidos, prop.usuario, prop.correo, prop.clave,)
   return await setData(query, params)
 
+# crud --------------------------------------------------------------------
 @app.get("/usuario")
 async def get_usuario_all():
   query = "pa_usuario_get_all"
-  params = []
+  params = ()
   return await getData(query, params)
 
 @app.get("/usuario/{id}")
 async def get_usuario_byid(id: int):
-  result = list(filter(lambda el: el.id == id, usuarios))
-  return result
+  return ""
 
+@app.post("/usuario")
+async def post_usuario_byid(props: Usuario):
+  return Usuario
+
+@app.put("/usuario")
+async def put_usuario_byid(props: Usuario):
+  return Usuario
+
+@app.delete("/usuario/{id}")
+async def del_usuario_byid(id: int):
+  return Usuario

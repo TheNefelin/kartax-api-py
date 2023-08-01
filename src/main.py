5871e204d73dcb9ced1a41dcd72bec7671e20330
Francisco Carmona <flcarmonac@yahoo.com>
2023-08-01 19:27:15 -0400

    login

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from routers import negocios, usuarios
 
 app = FastAPI(title="Kartax", description="API", version="3.0")
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="logearse")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="usuario-logearse")
 
 # origins = [
 #     "http://localhost",
@@ -40,7 +40,16 @@ class Logearse(BaseModel):
   clave: str
 
 # root --------------------------------------------------------------------
-@app.post("/logearse")
+@app.get("/", tags=["root"])
+async def root():
+  return await getTesting()
+
+@app.post("/usuario-registrarse", tags=["root"])
+async def registrarse(obj: Registrarse):
+  result = await execute_sp("pa_usuario_registrarse", (obj.nombres, obj.apellidos, obj.usuario, obj.correo, obj.clave,))
+  return result
+
+@app.post("/usuario-logearse", tags=["root"])
 async def iniciar_sesion(form: OAuth2PasswordRequestForm = Depends()):
   result = await execute_sp("pa_usuario_logearse", (form.username, form.password,))
   estado, msge, sql_token = result[0]['estado'], result[0]['msge'], result[0]['sql_token']
@@ -51,22 +60,12 @@ async def iniciar_sesion(form: OAuth2PasswordRequestForm = Depends()):
       detail = msge,
       headers={"WWW-Authenticate": "Bearer"},)
 
-  return {"access_token": sql_token, "token_type": "bearer", "msge": msge}
+  return {"access_token": f"{form.username}={sql_token}", "token_type": "bearer", "msge": msge}
 
-@app.post("/test")
-async def test(token: str = Depends(oauth2_scheme)):
+@app.get("/token", tags=["root"])
+async def get_token(token: str = Depends(oauth2_scheme)):
    print(token)
-   return {"msge": "Aguna Wea"}
-   
-
-# @app.get("/", tags=["root"])
-# async def root():
-#   return await getTesting()
-
-# @app.post("/usuario-registrarse", tags=["root"])
-# async def registrarse(obj: Registrarse):
-#   result = await execute_sp("pa_usuario_registrarse", (obj.nombres, obj.apellidos, obj.usuario, obj.correo, obj.clave,))
-#   return result
+   return {"token": token}
 
 # routers -----------------------------------------------------------------
 app.include_router(negocios.router, dependencies=[Depends(oauth2_scheme)])
